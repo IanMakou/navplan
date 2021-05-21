fa16996872606687f050d4aa06727d86e2015b62
IanMakou <IanFuturetech@live.com>
2021-05-21 13:24:29 +0200

    Commit

--- index.php
+++ index.php
@@ -3,16 +3,20 @@
 
 	$host = "www.navplan.ch";
 
-    if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off" || $_SERVER['HTTP_HOST'] != $host)
-    {
-        $redirect = 'https://' . $host . $_SERVER['REQUEST_URI'];
-        header('HTTP/1.1 301 Moved Permanently');
-        header('Location: ' . $redirect);
-    }
-    else
-    {
-        header("Cache-Control: public, max-age=60"); // max 1 min (must be public for appcache to work)
-    }
+    echo 'Hello world';
+
+    // $host = 
+
+    // if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off" || $_SERVER['HTTP_HOST'] != $host)
+    // {
+    //     $redirect = 'https://' . $host . $_SERVER['REQUEST_URI'];
+    //     header('HTTP/1.1 301 Moved Permanently');
+    //     header('Location: ' . $redirect);
+    // }
+    // else
+    // {
+    //     header("Cache-Control: public, max-age=60"); // max 1 min (must be public for appcache to work)
+    // }
 ?><!DOCTYPE HTML>
 <html lang="de" data-ng-app="navplanApp" data-ng-controller="navplanCtrl">
 <head>
